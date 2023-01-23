bf0a86a0cdc833503c2ed00516ce2820f466a460
Realnotema <100119630+Realnotema@users.noreply.github.com>
2023-01-24 02:41:11 +0300

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -36,3 +36,33 @@ TEST(Methods, Pocket) {
     ASSERT_EQ(pocket.is_download(), false);
     ASSERT_EQ(pocket.get_type(), 2);
 }
+
+TEST(Methods, PocketLibrary) {
+    std::vector<AbstractPocket *> vector;
+    PocketLibrary *pocket = nullptr;
+    ASSERT_EQ(pocket->get_type(), 3);
+    pocket->plus_count();
+    ASSERT_EQ(pocket->get_count_of_dependency(), 1);
+    pocket->minus_count();
+    ASSERT_EQ(pocket->get_count_of_dependency(), 0);
+}
+
+TEST(Methods, PocketManager) {
+    std::vector<AbstractPocket *> vector;
+    PocketManager *manager = nullptr;
+    Pocket pocket(false, "pocket", 2, 1, "name", vector);
+    manager->add_to_map(&pocket);
+    ASSERT_EQ(manager->is_in_map((string &) "pocket"), 0);
+    manager->installation("pocket");
+    ASSERT_EQ(pocket.is_download(), 0);
+    manager->delete_from_map(&pocket);
+    ASSERT_EQ(manager->is_in_map((string &) "pocket"), 1);
+    manager->system_update();
+    Info info = pocket.get_info();
+    ASSERT_EQ(info.version, 2);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
